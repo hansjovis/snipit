c778a291ced9dfbf2a2faa068cf91932206acef2
Hans-Christiaan Braun <hc.braun.93@gmail.com>
2020-09-06 15:37:10 +0200

    Added second strategy for gathering the snippet's image: first content image.

--- src/scripts/content/app.js
+++ src/scripts/content/app.js
@@ -23,7 +23,7 @@ import gatherSnippet from "./gather/snippet";
 		 * Gather and save a snippet of the current page.
 		 */
 		function snipit() {
-			const snippet = gatherSnippet( window.location.href, document.head );
+			const snippet = gatherSnippet( document );
 			saveSnippet( snippet );
 		}
 
--- src/scripts/content/gather/image.js
+++ src/scripts/content/gather/image.js
@@ -1,17 +1,48 @@
+
 /**
- * Gather the image from the given head of a web page.
- *
- * @param {HTMLElement} head The head of a web page.
- *
- * @return {string} The URL to the image.
+ * Gathers the URL to the image from the first `og:image` meta tag
+ * in the given HTML element.
+ * 
+ * @param {HTMLElement} element The element to gather the image from.
+ * 
+ * @returns {string} The URL to the image.
  */
-function gatherImage( head ) {
-	const imageElement = head.querySelector( "meta[property='og:image']" );
-	if ( imageElement ) {
-		return imageElement.getAttribute( "content" );
-	} else {
-		return "";
+function fromMeta( element ) {
+	const meta = element.querySelector( "meta[property='og:image']" );
+	if( meta ) {
+		const url = meta.getAttribute( "content" );
+		return new URL( url, window.location.origin ).toString();
 	}
+	return false;
+}
+
+/**
+ * Gathers the URL to the first image on the given HTML element. 
+ * 
+ * @param {HTMLElement} element The element to gather the image from.
+ * 
+ * @returns {string} The URL to the image.
+ */
+function firstContentImage( element ) {
+	const image = element.querySelector( "img" );
+	if ( image ) {
+		const url = image.getAttribute( "src" );
+		return new URL( url, window.location.origin ).toString();
+	}
+	return false;
+}
+
+/**
+ * Gather the image from the given web page.
+ *
+ * @param {HTMLElement} element The web page.
+ *
+ * @returns {string} The URL to the image.
+ */
+function gatherImage( element ) {
+	return fromMeta( element )
+		|| firstContentImage( element )
+		|| "";
 }
 
 export default gatherImage;
